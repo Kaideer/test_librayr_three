38376cd4f7b59a1d57db93bf5c61b6f815cc1e78
Kaideer <2024468244@qq.com>
2019-10-10 10:00:45 +0800

    测试

---
+++ my_library/src/main/java/com/kaider/my_library/ToastUtils.java
@@ -0,0 +1,18 @@
+package com.kaider.my_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author： KaiDer
+ * @date： 2019/10/10 9:40
+ * @description:
+ * @email： 2024468244@qq.com
+ */
+public class ToastUtils {
+
+    public void toastLong(Context context, String msg) {
+        Toast.makeText(context, msg + "", Toast.LENGTH_LONG).show();
+    }
+
+}
